44f1debe0dd59ed29448c79ce789e4456ba14140
Keny Dutton-Gillespie <dutton-gillespie.keny@student.greenriver.edu>
2022-05-07 23:25:50 -0400

    update grid

--- index.php
+++ index.php
@@ -4,6 +4,9 @@
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
+//Start a session
+session_start();
+
 // Require the autoload file
 require_once('vendor/autoload.php');
 
@@ -11,12 +14,20 @@ require_once('vendor/autoload.php');
 $f3 = Base::instance();
 
 // Define a default route
-$f3->route('GET /',function($f3) {
+$f3->route('GET /',function() {
 
     $view = new Template();
-    echo $view->render('views/info.html');
+    echo $view->render('views/home.html');
 
 });
+
+// Define a menu route
+$f3->route('POST|GET /menu', function() {
+
+    $view  = new Template();
+    echo $view->render('views/menu.html');
+});
+
 
 // Run fat free
 $f3->run();
